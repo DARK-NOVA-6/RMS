796299c5953b0467e13f48af22659cf74e3cd8b3
DARK-NOVA-6 <111617715+DARK-NOVA-6@users.noreply.github.com>
2022-08-19 18:46:02 -0700

    hello

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      title: 'RMS Demo',
+      title: 'RMS  WTF Demo',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
