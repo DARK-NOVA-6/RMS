b0456a984f57077d4731d43ab08b43559914a3c1
Mohamad-AlAswad <creative12.syr@gmail.com>
2022-08-20 04:36:59 +0300

    title

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'RMS Demo',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
